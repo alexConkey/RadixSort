a68ecd27df416eab4f805cfab0776dbd9aa6209e
Alex Conkey <aconkey20@stuy.edu>
2021-01-14 08:01:26 -0500

    Finished nth

--- Radix.java
+++ Radix.java
@@ -1,5 +1,11 @@
+import java.lang.Math;
 public class Radix {
-  public static int nth(int n, int col){}
+  public static int nth(int n, int col){
+    if (n<0){
+      n = n * -1;
+    }
+    return(((int)(n/Math.pow(10, col)))%10);
+  }
 
   public static int length(int n){}
 
