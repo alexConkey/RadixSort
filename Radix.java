8a4dbd4669bd474433dcf10f33bff06fbbe9897e
Alex Conkey <aconkey20@stuy.edu>
2021-01-19 09:20:34 -0500

    set up method headers

--- Radix.java
+++ Radix.java
@@ -11,12 +11,17 @@ public class Radix {
     if (n<0){
       n = n * -1;
     }
-    return((int)(Math.log10(n)+1)));
+    return((int)(Math.log10(n)+1));
   }
 
-  public static void merge(MyLinkedList original,MyLinkedList[]buckets){
+  public static void merge(SortableLinkedList original,SortableLinkedList[]buckets){
     for (int i = 0; i < buckets.length; i++){
       original.extend(buckets[i]);
     }
+  }
+  public static void radixSortSimple(SortableLinkedList data) {
+  }
+
+  public static void radixSort(SortableLinkedList data) {
   }
 }
